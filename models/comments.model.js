29c3d3d1cf716028055a596ba086b9d073ea1678
Neel1992004 <pneel1924@gmail.com>
2025-09-24 22:23:04 +0530

    Added multiple models (User,Post,Profile,Comment,Connection)

---
+++ models/comments.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const CommentScehma = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+});
+
+const Comment = mongoose.model('Comment', CommentScehma);
+
+export default Comment;
