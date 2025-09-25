5a26fe8ee260c80343ee97cdee2e2015c280a725
Neel1992004 <pneel1924@gmail.com>
2025-09-25 11:41:12 +0530

    Added postRoutes in server.s file

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import postRoutes from './routes/posts.routes.js';
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+
+app.use(postRoutes);
 
 app.use(express.json());
 
